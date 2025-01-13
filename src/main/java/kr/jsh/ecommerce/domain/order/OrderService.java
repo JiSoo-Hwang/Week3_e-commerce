dbfac7a6d1d90bed33eb8ded7023c0bc13fb8133
jisoo0507 <jisoo0507@naver.com>
2025-01-13 15:31:09 +0900

    feat✨: OrderService 생성

---
+++ src/main/java/kr/jsh/ecommerce/domain/order/OrderService.java
@@ -0,0 +1,11 @@
+package kr.jsh.ecommerce.domain.order;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class OrderService {
+
+    private final OrderRepository orderRepository;
+}
