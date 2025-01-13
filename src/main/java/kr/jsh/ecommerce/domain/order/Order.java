a5992119412b93ef631fa933b84f602f5167b32f
jisoo0507 <jisoo0507@naver.com>
2025-01-13 12:49:46 +0900

    feat✨:Order(주문),OrderFruit(주문한과일) 엔터티

--- src/main/java/kr/jsh/ecommerce/domain/order/Order.java
+++ src/main/java/kr/jsh/ecommerce/domain/order/Order.java
@@ -1,7 +1,14 @@
 package kr.jsh.ecommerce.domain.order;
 
 import jakarta.persistence.*;
+import kr.jsh.ecommerce.domain.customer.Customer;
+import kr.jsh.ecommerce.domain.payment.Payment;
 import lombok.*;
+
+import java.util.ArrayList;
+import java.util.Currency;
+import java.util.Date;
+import java.util.List;
 
 @Entity
 @Getter
@@ -14,5 +21,27 @@ public class Order {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long orderId;
 
+    @ManyToOne
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customer;
 
+    @Column(nullable = false)
+    private Date orderDate;
+
+    @Column(nullable = false)
+    private int totalAmount;
+
+    @Column(nullable = false)
+    private String orderStatus;
+
+    @OneToMany(mappedBy = "order", cascade=CascadeType.PERSIST)
+    private List<OrderFruit> orderFruits = new ArrayList<>();
+
+    @OneToOne(mappedBy = "order", cascade = CascadeType.PERSIST)
+    private Payment payment;
+
+    public void addOrderFruit(OrderFruit orderFruit) {
+        this.orderFruits.add(orderFruit);
+        orderFruit.setOrder(this);
+    }
 }
--- src/main/java/kr/jsh/ecommerce/domain/order/OrderFruit.java
+++ src/main/java/kr/jsh/ecommerce/domain/order/OrderFruit.java
@@ -1,4 +1,46 @@
 package kr.jsh.ecommerce.domain.order;
 
+import jakarta.persistence.*;
+import kr.jsh.ecommerce.domain.customer.Customer;
+import kr.jsh.ecommerce.domain.fruit.Fruit;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "order_fruit")
 public class OrderFruit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long orderFruitId;
+
+    @ManyToOne
+    @JoinColumn(name = "order_id", nullable = false)
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "customer_id", nullable = false)
+    private Customer customer;
+
+    @ManyToOne
+    @JoinColumn(name = "fruit_id", nullable = false)
+    private Fruit fruit;
+
+    @Column(nullable = false)
+    private int fruitPrice;
+
+    @Column(nullable = false)
+    private int quantity;
+
+    @Column(nullable = false)
+    private int subTotal;
+
+    public void setOrder(Order order) {
+        this.order = order;
+    }
 }
