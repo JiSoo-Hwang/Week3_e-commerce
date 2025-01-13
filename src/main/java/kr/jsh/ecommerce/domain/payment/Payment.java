0cb02e29ebcd4edfa57defb155ec1f06aa71de4a
jisoo0507 <jisoo0507@naver.com>
2025-01-13 12:50:32 +0900

    feat✨:Payment(결제) 엔터티

---
+++ src/main/java/kr/jsh/ecommerce/domain/payment/Payment.java
@@ -0,0 +1,18 @@
+package kr.jsh.ecommerce.domain.payment;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "payment")
+public class Payment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long paymentId;
+}
