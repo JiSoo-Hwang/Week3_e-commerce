9ec934478b11ad03f21c725cfe7bfc2a77dcfbb4
jisoo0507 <jisoo0507@naver.com>
2025-01-16 21:29:37 +0900

    feat✨:쿠폰발급 요청/응답 DTO

--- src/main/java/kr/jsh/ecommerce/interfaces/dto/coupon/CouponIssueRequest.java
+++ src/main/java/kr/jsh/ecommerce/interfaces/dto/coupon/CouponIssueRequest.java
@@ -1,9 +1,10 @@
 package kr.jsh.ecommerce.interfaces.dto.coupon;
 
 import kr.jsh.ecommerce.domain.coupon.Coupon;
+import kr.jsh.ecommerce.domain.customer.Customer;
 
 public record CouponIssueRequest(
         Coupon coupon,
-        Long customerId
+        Customer customer
 ) {
 }
