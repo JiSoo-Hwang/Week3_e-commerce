2b7f26c866e237ce9c221546ac669c47d73bb2fe
jisoo0507 <jisoo0507@naver.com>
2025-01-17 07:49:43 +0900

    chore🧹:테스트를 위한 환경 패키지 명시

--- src/main/java/kr/jsh/ecommerce/ServerApplication.java
+++ src/main/java/kr/jsh/ecommerce/ServerApplication.java
@@ -2,8 +2,10 @@ package kr.jsh.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication(scanBasePackages = "kr.jsh.ecommerce")
+@SpringBootApplication
+@EnableJpaRepositories(basePackages = "kr.jsh.ecommerce.infrastructure")
 public class ServerApplication {
 
 	public static void main(String[] args) {
