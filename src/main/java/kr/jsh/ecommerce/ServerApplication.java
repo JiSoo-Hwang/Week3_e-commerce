85a9e1a65d91c1fc7e955332a0b854dbd1c025ab
jisoo0507 <jisoo0507@naver.com>
2025-01-17 00:26:21 +0900

    chore🧹:테스트를 위한 환경 설정

--- src/main/java/kr/jsh/ecommerce/ServerApplication.java
+++ src/main/java/kr/jsh/ecommerce/ServerApplication.java
@@ -3,7 +3,7 @@ package kr.jsh.ecommerce;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "kr.jsh.ecommerce")
 public class ServerApplication {
 
 	public static void main(String[] args) {
