0e7b7f5995e2750f29bbae59925ef672d84a766c
jisoo0507 <jisoo0507@naver.com>
2025-01-28 14:22:19 +0900

    chore🧹: 주석 수정

--- src/main/java/kr/jsh/ecommerce/interfaces/api/order/controller/OrderController.java
+++ src/main/java/kr/jsh/ecommerce/interfaces/api/order/controller/OrderController.java
@@ -27,7 +27,7 @@ public class OrderController {
     @Parameter(name="orderCreateRequest",description = "고객 정보와 개별 과일들에 대한 주문 정보", required = true)
     @PostMapping
     public ResponseEntity<BaseResponseContent> createOrder(@RequestBody OrderCreateRequest orderCreateRequest){
-        OrderCreateResponse response = createOrderUseCase.createOrder(orderCreateRequest);//TODO:굳이 여기서 ID랑 주문한 과일을 빼야 했을깡,,,?
+        OrderCreateResponse response = createOrderUseCase.createOrder(orderCreateRequest);
         BaseResponseContent responseContent = new BaseResponseContent(response);
         responseContent.setMessage("주문 성공! 이제 결제해주세요 :)");
         return ResponseEntity
