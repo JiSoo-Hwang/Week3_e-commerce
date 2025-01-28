a24a9fe1edfab0fed384704c71ad356aa3827dc7
jisoo0507 <jisoo0507@naver.com>
2025-01-29 01:31:18 +0900

    chore🧹:JpaRepository 테스트 실행시 test 환경 별도 설정

--- src/test/java/kr/jsh/ecommerce/infrastructure/fruit/FruitJpaRepositoryTest.java
+++ src/test/java/kr/jsh/ecommerce/infrastructure/fruit/FruitJpaRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.MySQLContainer;
@@ -18,6 +19,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @DataJpaTest
 @Testcontainers
+@ActiveProfiles("test")
 class FruitJpaRepositoryTest {
 
     @Container
