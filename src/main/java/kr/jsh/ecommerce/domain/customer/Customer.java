a496623e294ed86cf377f8593b04da87619850ac
jisoo0507 <jisoo0507@naver.com>
2025-01-29 22:47:30 +0900

    refactor♻️: Customer 객체 팩토리 메서드 추가

--- src/main/java/kr/jsh/ecommerce/domain/customer/Customer.java
+++ src/main/java/kr/jsh/ecommerce/domain/customer/Customer.java
@@ -2,14 +2,15 @@ package kr.jsh.ecommerce.domain.customer;
 
 import jakarta.persistence.*;
 import kr.jsh.ecommerce.domain.wallet.Wallet;
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Getter
-@NoArgsConstructor
-@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED) // JPA 기본 생성자
+@AllArgsConstructor(access = AccessLevel.PRIVATE) // 모든 필드 초기화 생성자 (외부에서 직접 사용 불가)
 @Table(name = "customer")
 public class Customer {
 
@@ -29,9 +30,23 @@ public class Customer {
     @OneToOne(mappedBy = "customer")
     private Wallet wallet;
 
+    public static Customer create(String customerName,String customerAddress,String customerPhone) {
+        Customer customer = new Customer();
+        customer.customerName = customerName;
+        customer.customerAddress = customerAddress;
+        customer.customerPhone = customerPhone;
+        return customer;
+    }
+
     public static Customer create(Long customerId, String customerName) {
         Customer customer = new Customer();
         customer.customerId = customerId;
+        customer.customerName = customerName;
+        return customer;
+    }
+
+    public static Customer create(String customerName) {
+        Customer customer = new Customer();
         customer.customerName = customerName;
         return customer;
     }
