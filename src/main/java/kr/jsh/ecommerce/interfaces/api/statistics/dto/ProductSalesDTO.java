5b267c4c9822db1bfca44490cea069fb615fc54c
jisoo0507 <jisoo0507@naver.com>
2025-02-01 13:04:33 +0900

    refactor♻️: JPQL과 호환이 되기 위해 ProductSalesDTO record에서 class로 변환

--- src/main/java/kr/jsh/ecommerce/interfaces/api/statistics/dto/ProductSalesDTO.java
+++ src/main/java/kr/jsh/ecommerce/interfaces/api/statistics/dto/ProductSalesDTO.java
@@ -1,8 +1,21 @@
 package kr.jsh.ecommerce.interfaces.api.statistics.dto;
 
-public record ProductSalesDTO(
-        Long productId,
-        String productName,
-        Integer totalQuantity
-) {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor // 기본 생성자 필수!
+@AllArgsConstructor
+public class ProductSalesDTO {
+    private Long productId;
+    private String productName;
+    private int totalQuantity;
+
+    //  JPQL에서 사용할 생성자 명시
+    public ProductSalesDTO(Long productId, String productName, Long totalQuantity) {
+        this.productId = productId;
+        this.productName = productName;
+        this.totalQuantity = totalQuantity.intValue(); // JPQL SUM() 결과를 int로 변환
+    }
 }
