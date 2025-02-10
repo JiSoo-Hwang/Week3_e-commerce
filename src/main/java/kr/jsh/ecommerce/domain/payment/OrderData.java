e77c097d5a6dfb76ddfd381082ec7ab440998d5b
jisoo0507 <jisoo0507@naver.com>
2025-02-11 01:21:03 +0900

    feat✨: 결제 완료 후 가상의 데이터 플랫폼에 주문 정보 전달

---
+++ src/main/java/kr/jsh/ecommerce/domain/payment/OrderData.java
@@ -0,0 +1,20 @@
+package kr.jsh.ecommerce.domain.payment;
+
+import kr.jsh.ecommerce.domain.order.Order;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public class OrderData {
+    private final Long orderId;
+    private final LocalDateTime orderDate;
+    private final int totalAmount;
+
+    public static OrderData from(final Order order){
+        return new OrderData(order.getOrderId(),order.getOrderDate(),order.getTotalAmount());
+    }
+}
