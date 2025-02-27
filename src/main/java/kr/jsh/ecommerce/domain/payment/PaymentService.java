390c09133a57f6f4b4b5a453cdc17c1777b5db5b
jisoo0507 <jisoo0507@naver.com>
2025-02-27 22:02:13 +0900

    fix/refactor🛠️♻️: 기존 주문/결제 로직 수정

--- src/main/java/kr/jsh/ecommerce/domain/payment/PaymentService.java
+++ src/main/java/kr/jsh/ecommerce/domain/payment/PaymentService.java
@@ -62,18 +62,10 @@ public class PaymentService {
             wallet.spendCash(finalAmount);
             payment.completePayment();
         } catch (BaseCustomException e) {
-            // 예외 발생 시 결제 실패 처리
-            markPaymentAsFail(payment);
+            // 결제 실패시 예외 던지기
             throw e;
         }
         return payment;
-    }
-
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
-    public void markPaymentAsFail(Payment payment){
-        payment.failPayment();
-        paymentRepository.save(payment);
-        paymentRepository.flush();
     }
 
     public Order updateOrderStatus(Order order, Payment payment) {
@@ -90,41 +82,4 @@ public class PaymentService {
             throw new RuntimeException("Kafka 메시지 전송 실패", e);
         }
     }
-
-/*
-    @Transactional
-    public Payment createPayment(Order order, CouponIssue issuedCoupon) {
-        Wallet wallet = order.getCustomer().getWallet();
-        // 쿠폰 할인 적용
-        int discount = 0;
-        if (issuedCoupon != null) {
-            issuedCoupon.markAsUsed();  // 쿠폰 사용 처리
-            discount = issuedCoupon.getCoupon().getDiscountAmount();
-        }
-        // 최종 결제 금액 계산
-        int finalAmount = Math.max(0, order.getTotalAmount() - discount);
-
-        //결제 객체 생성
-        Payment payment = Payment.create(order, finalAmount);
-
-        //잔액 차감
-        try {
-            // 결제 시도
-            wallet.spendCash(finalAmount);
-            payment.completePayment();
-        } catch (BaseCustomException e) {
-            // 예외 발생 시 결제 실패 처리
-            payment.failPayment();
-        }
-
-        dataPlatFormClient.sendOrderData(OrderData.from(order));
-
-        //결제 정보 저장
-        paymentRepository.save(payment);
-        order.setPayment(payment);
-        orderRepository.save(order);
-
-        return payment;
-    }
- */
 }
